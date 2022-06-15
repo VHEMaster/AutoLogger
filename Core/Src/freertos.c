fdd16b1636593add748913e4fae30bcaf3d0c4df
Maksym Naumchuk <vhemaster98@gmail.com>
2022-06-15 21:47:13 +0300

    Extend storage logic

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -41,15 +41,29 @@ osThreadId_t h_task_kline;
 
 volatile uint64_t gTick64 = 0;
 
+char *uint64_to_str(uint64_t n, char dest[static 21]) {
+    dest += 20;
+    *dest-- = 0;
+    while (n) {
+        *dest-- = (n % 10) + '0';
+        n /= 10;
+    }
+    return dest + 1;
+}
+
+
+#define LOG10_FROM_2_TO_64_PLUS_1  21
+#define UINT64_TO_STR(n)  uint64_to_str(n, (char[21]){0})
+
 const char *gFileHeader = "TimePoint,CurrentTableName,SwitchPosition,CurrentTable,InjectorChannel,AdcKnockVoltage,AdcAirTemp,AdcEngineTemp,"
-    "AdcManifoldAirPressure,AdcThrottlePosition,AdcPowerVoltage,AdcReferenceVoltage,AdcLambdaUR,AdcLambdaUA,KnockSensor,KnockSensorFiltered,"
-    "AirTemp,EngineTemp,ManifoldAirPressure,ThrottlePosition,ReferenceVoltage,PowerVoltage,FuelRatio,LambdaValue,LambdaTemperature,"
-    "LambdaHeaterVoltage,LambdaTemperatureVoltage,ShortTermCorrection,LongTermCorrection,IdleCorrection,IdleFlag,RPM,Speed,Acceleration,"
-    "MassAirFlow,CyclicAirFlow,EffectiveVolume,AirDestiny,RelativeFilling,WishFuelRatio,IdleValvePosition,WishIdleRPM,WishIdleMassAirFlow,"
-    "WishIdleValvePosition,WishIdleIgnitionAngle,IgnitionAngle,InjectionPhase,InjectionPhaseDuration,InjectionPulse,InjectionDutyCycle,"
-    "InjectionEnrichment,InjectionLag,IgnitionPulse,IdleSpeedShift,DrivenKilometers,FuelConsumed,FuelConsumption,FuelHourly,TspsRelativePosition,"
-    "LambdaValid,OilSensor,StarterSensor,HandbrakeSensor,ChargeSensor,ClutchSensor,IgnSensor,FuelPumpRelay,FanRelay,CheckEngine,StarterRelay,"
-    "Rsvd1Output,IgnOutput,StartAllowed,IsRunning,IsCheckEngine\r\n";
+  "AdcManifoldAirPressure,AdcThrottlePosition,AdcPowerVoltage,AdcReferenceVoltage,AdcLambdaUR,AdcLambdaUA,KnockSensor,KnockSensorFiltered,"
+  "AirTemp,EngineTemp,ManifoldAirPressure,ThrottlePosition,ReferenceVoltage,PowerVoltage,FuelRatio,LambdaValue,LambdaTemperature,"
+  "LambdaHeaterVoltage,LambdaTemperatureVoltage,ShortTermCorrection,LongTermCorrection,IdleCorrection,IdleFlag,RPM,Speed,Acceleration,"
+  "MassAirFlow,CyclicAirFlow,EffectiveVolume,AirDestiny,RelativeFilling,WishFuelRatio,IdleValvePosition,WishIdleRPM,WishIdleMassAirFlow,"
+  "WishIdleValvePosition,WishIdleIgnitionAngle,IgnitionAngle,InjectionPhase,InjectionPhaseDuration,InjectionPulse,InjectionDutyCycle,"
+  "InjectionEnrichment,InjectionLag,IgnitionPulse,IdleSpeedShift,DrivenKilometers,FuelConsumed,FuelConsumption,FuelHourly,TspsRelativePosition,"
+  "LambdaValid,OilSensor,StarterSensor,HandbrakeSensor,ChargeSensor,ClutchSensor,IgnSensor,FuelPumpRelay,FanRelay,CheckEngine,StarterRelay,"
+  "Rsvd1Output,IgnOutput,StartAllowed,IsRunning,IsCheckEngine\r\n";
 
 /*
  * 0 = uint64_t
@@ -58,11 +72,14 @@ const char *gFileHeader = "TimePoint,CurrentTableName,SwitchPosition,CurrentTabl
  * 3 = float
  */
 const uint8_t gFileFormats[] = {
-    0,1,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
-    3,3,3,3,3,3,3,3,3,3,2,3,3,3,3,3,3,3,3,3,
-    3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,
-    2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
-  };
+  1,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,3,3,2,3,3,3,3,3,3,3,3,3,
+  3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,
+  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+};
+
+#define PARAMS_BUFFER_SIZE 32
+static sParameters gParamsBuffer[2][PARAMS_BUFFER_SIZE];
 
 const osThreadAttr_t attrs_task_time = {
   .name = "taskTime",
@@ -112,173 +129,179 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   }
 }
 
-void StartUsbTask(void *argument)
-{
-  uint8_t state = 0;
-  uint8_t state_prev = 0;
-  uint8_t initialized = 0;
-  FRESULT fres;
-  char filename[32];
-  char string[128];
-  FATFS *fatfs = &USBHFatFS;
-  FIL *file = &USBHFile;
-  const char *path = USBHPath;
-  uint16_t filenumber = 0;
-  UINT read;
-  UINT wrote;
-  uint8_t recreate = 0;
-  sParameters parameters = {0};
-  uint64_t InitTime64 = 0;
+struct sLogDriver {
+    uint8_t initialized;
+    uint8_t was_initialized;
+    FRESULT fres;
+    char filename[32];
+    char string[128];
+    uint16_t filenumber;
+    UINT read;
+    UINT wrote;
+    sParameters parameters;
+    uint64_t InitTime64;
+    uint64_t diff;
 
-  MX_USB_HOST_Init();
+    eLed led;
+    FATFS *fatfs;
+    FIL *file;
+    const char *path;
+    sProFIFO fifo;
+};
+
+static void driver_loop(struct sLogDriver *driver)
+{
+  led_set(driver->led, LedOff);
+  led_set_post(driver->led, LedOff);
 
   for(;;)
   {
-    state = USBH_GetAppliState() == APPLICATION_READY;
-    if(state) {
-      if(!initialized) {
-        fres = f_mount(fatfs, path, 1);
-        if(fres != FR_OK) {
-          osDelay(100);
-          continue;
-        }
+    if(!driver->initialized) {
 
-        recreate = 0;
-        sprintf(filename, "%s/last.txt", path);
-        fres = f_open(file, filename, FA_OPEN_EXISTING | FA_READ);
-        if(fres == FR_OK) {
-          if(f_size(file) >= sizeof(string)) {
-            fres = f_close(file);
-            if(fres != FR_OK) { osDelay(100); continue; }
-            filenumber = 1;
-            recreate = 1;
-          } else {
-            fres = f_read(file, string, f_size(file), &read);
-            if(fres != FR_OK) { f_close(file); osDelay(100); continue; }
-            fres = f_close(file);
-            if(fres != FR_OK) { osDelay(100); continue; }
-            string[read] = 0;
-            fres = sscanf(string, "%hu", &filenumber);
-            if(fres != 1) {
-              recreate = 1;
-            }
-
-          }
-        }
-
-        if(recreate) {
-          sprintf(filename, "%s/last.txt", path);
-          fres = f_open(file, filename, FA_CREATE_ALWAYS | FA_WRITE);
-          if(fres == FR_OK) {
-            sprintf(filename, "%05hu", filenumber);
-            fres = f_write(file, filename, strlen(filename), &read);
-            if(fres != FR_OK) { f_close(file); osDelay(100); continue; }
-            fres = f_sync(file);
-            if(fres != FR_OK) { f_close(file); osDelay(100); continue; }
-            fres = f_close(file);
-            if(fres != FR_OK) { osDelay(100); continue; }
-          } else {
-            osDelay(100);
-            continue;
-          }
-        }
-
-        sprintf(filename, "%s/log_%05hu.eculog", path, filenumber);
-        fres = f_open(file, filename, FA_CREATE_ALWAYS | FA_WRITE);
-        if(fres != FR_OK) { osDelay(100); continue; }
-        fres = f_write(file, gFileHeader, strlen(gFileHeader), &wrote);
-        if(fres != FR_OK) { f_close(file); osDelay(100); continue; }
-        fres = f_sync(file);
-        if(fres != FR_OK) { f_close(file); osDelay(100); continue; }
-
-        initialized = 1;
-        InitTime64 = gTick64;
+      if(!driver->initialized && driver->was_initialized) {
+        f_close(driver->file);
+        f_mount(NULL, driver->path, 0);
+        driver->was_initialized = 0;
       }
 
-      //TODO: logic of I/O of parameters
-      if(1) {
-        sprintf(string, "%llu000,", gTick64 - InitTime64);
-        fres = f_write(file, string, strlen(string), &wrote);
-        if(fres != FR_OK) { f_close(file); initialized = 0; state_prev = 0; osDelay(100); continue; }
-        for(int i = 0, j = 0; i < sizeof(parameters) / sizeof(uint32_t); j++) {
-          string[0] = ',';
-          string[1] = '\0';
-          switch(gFileFormats[j]) {
-            case 1:
-              sprintf(string, "%s,", (const char *)(((uint32_t *)&parameters)[i]));
-              break;
-            case 2:
-              sprintf(string, "%ld,", ((const int32_t *)&parameters)[i]);
-              break;
-            case 3:
-              sprintf(string, "%f,", ((const float *)&parameters)[i]);
-              break;
-            default:
-              break;
+      driver->fres = f_mount(driver->fatfs, driver->path, 1);
+      if(driver->fres != FR_OK) {
+        osDelay(100);
+        continue;
+      }
+
+      led_set(driver->led, LedLong);
+      led_set_post(driver->led, LedOn);
+
+      sprintf(driver->filename, "%s/last.txt", driver->path);
+      driver->fres = f_open(driver->file, driver->filename, FA_OPEN_EXISTING | FA_READ);
+      if(driver->fres == FR_OK) {
+        if(f_size(driver->file) >= sizeof(driver->string)) {
+          driver->fres = f_close(driver->file);
+          if(driver->fres != FR_OK) { osDelay(100); continue; }
+          driver->filenumber = 1;
+        } else {
+          driver->fres = f_read(driver->file, driver->string, f_size(driver->file), &driver->read);
+          if(driver->fres != FR_OK) { f_close(driver->file); osDelay(100); continue; }
+          driver->fres = f_close(driver->file);
+          if(driver->fres != FR_OK) { osDelay(100); continue; }
+          driver->string[driver->read] = 0;
+          driver->fres = sscanf(driver->string, "%hu", &driver->filenumber);
+          if(driver->fres == 1) {
+            driver->filenumber++;
           }
-          if(i == 0) {
-            i += TABLE_STRING_MAX / sizeof(uint32_t);
-          } else {
-            i++;
-          }
-          if(i >= sizeof(sizeof(parameters) / sizeof(uint32_t))) {
-            string[strlen(string) - 1] = '\0';
-          }
-          fres = f_write(file, string, strlen(string), &wrote);
-          if(fres != FR_OK) { f_close(file); initialized = 0; state_prev = 0; osDelay(100); continue; }
 
         }
+      } else if(driver->fres == FR_NO_FILE) {
+        driver->filenumber = 1;
+      } else { osDelay(100); continue; }
+
+      driver->fres = f_open(driver->file, driver->filename, FA_CREATE_ALWAYS | FA_WRITE);
+      if(driver->fres == FR_OK) {
+        sprintf(driver->filename, "%05hu", driver->filenumber);
+        driver->fres = f_write(driver->file, driver->filename, strlen(driver->filename), &driver->read);
+        if(driver->fres != FR_OK) { f_close(driver->file); osDelay(100); continue; }
+        driver->fres = f_sync(driver->file);
+        if(driver->fres != FR_OK) { f_close(driver->file); osDelay(100); continue; }
+        driver->fres = f_close(driver->file);
+        if(driver->fres != FR_OK) { osDelay(100); continue; }
+      } else {
+        osDelay(100);
+        continue;
       }
 
-      strcpy(string, "\r\n");
-      fres = f_write(file, string, strlen(string), &wrote);
-      if(fres != FR_OK) { f_close(file); initialized = 0; state_prev = 0; osDelay(100); continue; }
-      fres = f_sync(file);
-      if(fres != FR_OK) { f_close(file); initialized = 0; state_prev = 0; osDelay(100); continue; }
+      sprintf(driver->filename, "%s/log_%05hu.eculog", driver->path, driver->filenumber);
+      driver->fres = f_open(driver->file, driver->filename, FA_CREATE_ALWAYS | FA_WRITE);
+      if(driver->fres != FR_OK) { osDelay(100); continue; }
+      driver->fres = f_write(driver->file, gFileHeader, strlen(gFileHeader), &driver->wrote);
+      if(driver->fres != FR_OK) { f_close(driver->file); osDelay(100); continue; }
+      driver->fres = f_sync(driver->file);
+      if(driver->fres != FR_OK) { f_close(driver->file); osDelay(100); continue; }
 
-    } else {
-      led_set(LedUsb, LedOff);
-      led_set_post(LedUsb, LedOff);
+      driver->initialized = 1;
+      driver->InitTime64 = gTick64;
 
-      if(state_prev != state && initialized) {
-        f_close(file);
-        f_mount(NULL, path, 0);
-        initialized = 0;
-      }
+      led_set(driver->led, LedOn);
+      led_set_post(driver->led, LedOn);
     }
 
-    state_prev = state;
+    if(protPull(&driver->fifo, &driver->parameters)) {
+      driver->diff = gTick64 - driver->InitTime64;
+      if(driver->diff > 0) {
+        sprintf(driver->string, "%s000,", UINT64_TO_STR(driver->diff));
+      } else {
+        strcpy(driver->string, "0,");
+      }
+      driver->fres = f_write(driver->file, driver->string, strlen(driver->string), &driver->wrote);
+      if(driver->fres != FR_OK) { f_close(driver->file); driver->initialized = 0; osDelay(100); continue; }
+      for(int i = 0, j = 0; i < sizeof(driver->parameters) / sizeof(uint32_t); j++) {
+        driver->string[0] = ',';
+        driver->string[1] = '\0';
+        switch(gFileFormats[j]) {
+          case 1:
+            sprintf(driver->string, "%s,", (const char *)&((uint32_t *)&driver->parameters)[i]);
+            break;
+          case 2:
+            sprintf(driver->string, "%ld,", ((const int32_t *)&driver->parameters)[i]);
+            break;
+          case 3:
+            sprintf(driver->string, "%f,", ((const float *)&driver->parameters)[i]);
+            break;
+          default:
+            break;
+        }
+        if(i == 0) {
+          i += TABLE_STRING_MAX / sizeof(uint32_t);
+        } else {
+          i++;
+        }
+        if(i >= sizeof(driver->parameters) / sizeof(uint32_t)) {
+          driver->string[strlen(driver->string) - 1] = '\0';
+        }
+        driver->fres = f_write(driver->file, driver->string, strlen(driver->string), &driver->wrote);
+        if(driver->fres != FR_OK) { f_close(driver->file); driver->initialized = 0; osDelay(100); continue; }
+
+      }
+
+      strcpy(driver->string, "\r\n");
+      driver->fres = f_write(driver->file, driver->string, strlen(driver->string), &driver->wrote);
+      if(driver->fres != FR_OK) { f_close(driver->file); driver->initialized = 0; osDelay(100); continue; }
+      driver->fres = f_sync(driver->file);
+      if(driver->fres != FR_OK) { f_close(driver->file); driver->initialized = 0; osDelay(100); continue; }
+
+      led_set(driver->led, LedShortSingle);
+    }
+
     osDelay(1);
   }
 }
 
+void StartUsbTask(void *argument)
+{
+  struct sLogDriver driver = {0};
+
+  MX_USB_HOST_Init();
+
+  driver.fatfs = &USBHFatFS;
+  driver.file = &USBHFile;
+  driver.path = USBHPath;
+  driver.led = LedUsb;
+  protInit(&driver.fifo, gParamsBuffer[0], sizeof(sParameters), PARAMS_BUFFER_SIZE);
+
+  driver_loop(&driver);
+}
+
 void StartSdioTask(void *argument)
 {
-  uint8_t initialized = 0;
-  uint8_t was_initialized = 0;
-  FRESULT fres;
-  FATFS *fatfs = &SDFatFS;
-  FIL *file = &SDFile;
-  const char *path = SDPath;
+  struct sLogDriver driver = {0};
 
-  for(;;)
-  {
-    if(!initialized && was_initialized) {
-      f_close(file);
-      f_mount(NULL, path, 0);
-      was_initialized = 0;
-    }
+  driver.fatfs = &SDFatFS;
+  driver.file = &SDFile;
+  driver.path = SDPath;
+  driver.led = LedSdio;
+  protInit(&driver.fifo, gParamsBuffer[1], sizeof(sParameters), PARAMS_BUFFER_SIZE);
 
-    if(!initialized) {
-      fres = f_mount(fatfs, path, 1);
-      if(fres != FR_OK) {
-        osDelay(100);
-        continue;
-      }
-      initialized = 1;
-      was_initialized = initialized;
-    }
-  }
+  driver_loop(&driver);
 }
 
 void StartCanTask(void *argument)
--- Middlewares/Third_Party/FatFs/src/drivers/usbh_diskio.c
+++ Middlewares/Third_Party/FatFs/src/drivers/usbh_diskio.c
@@ -1,6 +1,6 @@
 /**
   ******************************************************************************
-  * @file    usbh_diskio.c 
+  * @file    usbh_diskio.c
   * @author  MCD Application Team
   * @version V1.4.1
   * @date    14-February-2017
@@ -10,43 +10,44 @@
   *
   * <h2><center>&copy; COPYRIGHT 2017 STMicroelectronics</center></h2>
   *
-  * Redistribution and use in source and binary forms, with or without 
+  * Redistribution and use in source and binary forms, with or without
   * modification, are permitted, provided that the following conditions are met:
   *
-  * 1. Redistribution of source code must retain the above copyright notice, 
+  * 1. Redistribution of source code must retain the above copyright notice,
   *    this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright notice,
   *    this list of conditions and the following disclaimer in the documentation
   *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
   *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
+  * 4. This software, including modifications and/or derivative works of this
   *    software, must execute solely and exclusively on microcontroller or
   *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
   *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
   * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
   * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
   * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
   * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
-  */ 
+  */
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
 #include "ff_gen_drv.h"
+#include "usb_host.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -69,15 +70,15 @@ DRESULT USBH_read (BYTE, BYTE*, DWORD, UINT);
 #if _USE_IOCTL == 1
   DRESULT USBH_ioctl (BYTE, BYTE, void*);
 #endif /* _USE_IOCTL == 1 */
-  
+
 const Diskio_drvTypeDef  USBH_Driver =
 {
   USBH_initialize,
   USBH_status,
-  USBH_read, 
+  USBH_read,
 #if  _USE_WRITE == 1
   USBH_write,
-#endif /* _USE_WRITE == 1 */  
+#endif /* _USE_WRITE == 1 */
 #if  _USE_IOCTL == 1
   USBH_ioctl,
 #endif /* _USE_IOCTL == 1 */
@@ -92,7 +93,11 @@ const Diskio_drvTypeDef  USBH_Driver =
   */
 DSTATUS USBH_initialize(BYTE lun)
 {
-  return RES_OK;
+  ApplicationTypeDef state;
+
+  state = USBH_GetAppliState();
+
+  return state == APPLICATION_READY ? RES_OK : RES_ERROR;
 }
 
 /**
@@ -103,7 +108,7 @@ DSTATUS USBH_initialize(BYTE lun)
 DSTATUS USBH_status(BYTE lun)
 {
   DRESULT res = RES_ERROR;
-  
+
   if(USBH_MSC_UnitIsReady(&HOST_HANDLE, lun))
   {
     res = RES_OK;
@@ -112,12 +117,12 @@ DSTATUS USBH_status(BYTE lun)
   {
     res = RES_ERROR;
   }
-  
+
   return res;
 }
 
 /**
-  * @brief  Reads Sector(s) 
+  * @brief  Reads Sector(s)
   * @param  lun : lun id
   * @param  *buff: Data buffer to store read data
   * @param  sector: Sector address (LBA)
@@ -153,36 +158,36 @@ DRESULT USBH_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
   {
     status = USBH_MSC_Read(&HOST_HANDLE, lun, sector, buff, count);
   }
-  
+
   if(status == USBH_OK)
   {
     res = RES_OK;
   }
   else
   {
-    USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info); 
-    
+    USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info);
+
     switch (info.sense.asc)
     {
     case SCSI_ASC_LOGICAL_UNIT_NOT_READY:
     case SCSI_ASC_MEDIUM_NOT_PRESENT:
-    case SCSI_ASC_NOT_READY_TO_READY_CHANGE: 
-      USBH_ErrLog ("USB Disk is not ready!");  
+    case SCSI_ASC_NOT_READY_TO_READY_CHANGE:
+      USBH_ErrLog ("USB Disk is not ready!");
       res = RES_NOTRDY;
-      break; 
-      
+      break;
+
     default:
       res = RES_ERROR;
       break;
     }
   }
-  
+
   return res;
 }
 
 /**
   * @brief  Writes Sector(s)
-  * @param  lun : lun id 
+  * @param  lun : lun id
   * @param  *buff: Data to be written
   * @param  sector: Sector address (LBA)
   * @param  count: Number of sectors to write (1..128)
@@ -191,9 +196,9 @@ DRESULT USBH_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
 #if _USE_WRITE == 1
 DRESULT USBH_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
 {
-  DRESULT res = RES_ERROR; 
+  DRESULT res = RES_ERROR;
   MSC_LUNTypeDef info;
-  USBH_StatusTypeDef  status = USBH_OK;  
+  USBH_StatusTypeDef  status = USBH_OK;
 
   if ((DWORD)buff & 3) /* DMA Alignment issue, do single up to aligned buffer */
   {
@@ -201,7 +206,7 @@ DRESULT USBH_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
     while (count--)
     {
       memcpy (scratch, &buff[count * _MAX_SS], _MAX_SS);
-      
+
       status = USBH_MSC_Write(&HOST_HANDLE, lun, sector + count, (BYTE *)scratch, 1) ;
       if(status == USBH_FAIL)
       {
@@ -216,36 +221,36 @@ DRESULT USBH_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
   {
     status = USBH_MSC_Write(&HOST_HANDLE, lun, sector, (BYTE *)buff, count);
   }
-  
+
   if(status == USBH_OK)
   {
     res = RES_OK;
   }
   else
   {
-    USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info); 
-    
+    USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info);
+
     switch (info.sense.asc)
     {
     case SCSI_ASC_WRITE_PROTECTED:
       USBH_ErrLog("USB Disk is Write protected!");
       res = RES_WRPRT;
       break;
-      
+
     case SCSI_ASC_LOGICAL_UNIT_NOT_READY:
     case SCSI_ASC_MEDIUM_NOT_PRESENT:
     case SCSI_ASC_NOT_READY_TO_READY_CHANGE:
-      USBH_ErrLog("USB Disk is not ready!");      
+      USBH_ErrLog("USB Disk is not ready!");
       res = RES_NOTRDY;
-      break; 
-      
+      break;
+
     default:
       res = RES_ERROR;
       break;
     }
   }
-  
-  return res;   
+
+  return res;
 }
 #endif /* _USE_WRITE == 1 */
 
@@ -261,16 +266,16 @@ DRESULT USBH_ioctl(BYTE lun, BYTE cmd, void *buff)
 {
   DRESULT res = RES_ERROR;
   MSC_LUNTypeDef info;
-  
+
   switch (cmd)
   {
   /* Make sure that no pending write process */
-  case CTRL_SYNC: 
+  case CTRL_SYNC:
     res = RES_OK;
     break;
-    
-  /* Get number of sectors on the disk (DWORD) */  
-  case GET_SECTOR_COUNT : 
+
+  /* Get number of sectors on the disk (DWORD) */
+  case GET_SECTOR_COUNT :
     if(USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info) == USBH_OK)
     {
       *(DWORD*)buff = info.capacity.block_nbr;
@@ -281,9 +286,9 @@ DRESULT USBH_ioctl(BYTE lun, BYTE cmd, void *buff)
       res = RES_ERROR;
     }
     break;
-    
-  /* Get R/W sector size (WORD) */  
-  case GET_SECTOR_SIZE :	
+
+  /* Get R/W sector size (WORD) */
+  case GET_SECTOR_SIZE :
     if(USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info) == USBH_OK)
     {
       *(DWORD*)buff = info.capacity.block_size;
@@ -294,10 +299,10 @@ DRESULT USBH_ioctl(BYTE lun, BYTE cmd, void *buff)
       res = RES_ERROR;
     }
     break;
-    
-    /* Get erase block size in unit of sector (DWORD) */ 
-  case GET_BLOCK_SIZE : 
-    
+
+    /* Get erase block size in unit of sector (DWORD) */
+  case GET_BLOCK_SIZE :
+
     if(USBH_MSC_GetLUNInfo(&HOST_HANDLE, lun, &info) == USBH_OK)
     {
       *(DWORD*)buff = info.capacity.block_size;
@@ -308,11 +313,11 @@ DRESULT USBH_ioctl(BYTE lun, BYTE cmd, void *buff)
       res = RES_ERROR;
     }
     break;
-    
+
   default:
     res = RES_PARERR;
   }
-  
+
   return res;
 }
 #endif /* _USE_IOCTL == 1 */
